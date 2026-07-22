7176c5f6a564cdd3ed07f1d7af434b7f3432a233
Palak Agarwal <palakagarwal0614@gmail.com>
2026-07-22 23:14:30 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.37%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-       
+        int xorr=0;
         for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-              if(nums[j]==nums[i]){
-                count++;
-              }
-            }
-            if(count==1){
-            return nums[i];
-        }
+            xorr=xorr^nums[i];
         }
         
-        return -1;
+        // for(int i=0;i<n;i++){
+        //     int count=0;
+        //     for(int j=0;j<n;j++){
+        //       if(nums[j]==nums[i]){
+        //         count++;
+        //       }
+        //     }
+        //     if(count==1){
+        //     return nums[i];
+        // }
+        // }
+        
+        return xorr;
     }
 };
