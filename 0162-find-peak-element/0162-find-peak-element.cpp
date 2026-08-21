fb36667bd8fbb193d0cecd49316460089c44a880
Palak Agarwal <palakagarwal0614@gmail.com>
2026-08-22 00:08:01 +0530

    Time: 0 ms (100%), Space: 12.7 MB (44.55%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -11,15 +11,17 @@ public:
         if(nums[n-1]>nums[n-2]){
             return n-1;
         }
-        int right=n-1;
-        int left=0;
+        int right=n-2;
+        int left=1;
         while(left<right){
             int mid=(left+right)/2;
-            if(nums[mid]>nums[mid+1]){
-                right=mid;
-            }else{
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }else if(nums[mid]<nums[mid+1]){
                 left=mid+1;
-            }
+            }else{
+            right=mid-1;
+           }
         }
         return left;
     }
